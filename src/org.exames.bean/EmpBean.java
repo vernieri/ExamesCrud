2e1fe64c40a066e098ff33617686dd75e89e02e4
Vernieri <jvmendes.ti@gmail.com>
2019-02-02 23:38:52 -0200

    Create EmpBean.java

---
+++ src/org.exames.bean/EmpBean.java
@@ -0,0 +1,47 @@
+package org.exames.bean;
+
+public class EmpBean {
+	// Bora gerar os meters e os seters?!
+	private String unome, ucpf, unota, ucargo;
+	int srNo;
+
+	public int getSrNo() {
+		return srNo;
+	}
+
+	public void setSrNo(int srNo) {
+		this.srNo = srNo;
+	}
+
+	public String getUnome() {
+		return unome;
+	}
+
+	public void setUname(String unome) {
+		this.unome = unome;
+	}
+
+	public String getUcpf() {
+		return ucpf;
+	}
+
+	public void setUcpf(String ucpf) {
+		this.ucpf = ucpf;
+	}
+
+	public String getUnota() {
+		return unota;
+	}
+
+	public void setUnota(String unota) {
+		this.unota= unota;
+	}
+
+	public String getUcargo() {
+		return ucargo;
+	}
+
+	public void setUcargo(String ucargo) {
+		this.ucargo = ucargo;
+	}
+}
