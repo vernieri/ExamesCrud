5edc8ee88e62e93e8885dc5c335dc4e5f821d123
Vernieri <jvmendes.ti@gmail.com>
2019-02-03 00:30:59 -0200

    Create struts2crud.sql

---
+++ WebContent/sql/struts2crud.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `struts2crud` (
+  `unome` VARCHAR(50) DEFAULT NULL,
+  `ucpf` VARCHAR(11) NOT NULL,
+  `unota` VARCHAR(6) DEFAULT NULL,
+  `ucargo` VARCHAR(25) DEFAULT NULL,
+  PRIMARY KEY (`ucpf`)
+);
